9ed4167a0b27fb89d3b5c26111e5391019e500a8
xqr <3490317344@qq.com>
2026-03-18 16:43:11 +0800

    重新修改前端

--- frontend/dist/assets/agent_python-BC0Fbcjd.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index-CagfOnBe.js";const a="http://localhost:8001",o=e.create({baseURL:a,timeout:6e4,headers:{"Content-Type":"application/json"}});async function p(t){return(await o.post("/agent/optimize",t)).data}async function r(t){return(await o.post("/agent/playground",t)).data}export{p as o,r as p};
